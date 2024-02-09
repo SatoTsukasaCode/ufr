5dedc4b0458ee6fe20f4da4a308ad1a532674d48
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-02-09 17:21:50 +0200

    all i did

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = args[1].parse::<String>().unwrap();
+    let file_inside = fs::read_to_string(file_path).expect("No File Detected In Specified Path");
+
+    println!("{file_inside}");
+}

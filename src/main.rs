df09276228f88030e0d5919eb46cc28f6d87f7cf
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-02-09 23:12:31 +0200

    whoops forgot to delete a variable

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = args[1].parse::<String>().unwrap();
-    let arg2 = args[2].parse::<String>().unwrap();
     let file_inside = fs::read_to_string(file_path.clone()).expect("No File Detected In Specified Path");
 
     println!("{file_inside}");

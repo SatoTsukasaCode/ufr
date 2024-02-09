c6616e0a06fa52275c5cb00463006700ae89a4de
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-02-09 23:11:28 +0200

    cleaned out arguments. gonna make this just a file reader

--- README.md
+++ README.md
@@ -7,8 +7,4 @@ A file reader I made in 5 minutes
 
 ## How to use
 
-`ufr <file path> <arguments>`
-
-current file arguments:
-
--s     Checks the file's size.
+`ufr <file path>`
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,6 @@ fn main() {
     let file_path = args[1].parse::<String>().unwrap();
     let arg2 = args[2].parse::<String>().unwrap();
     let file_inside = fs::read_to_string(file_path.clone()).expect("No File Detected In Specified Path");
-    let file_metadata = fs::metadata(file_path);
 
-    if arg2 == "-s" {
-        println!("{}", file_metadata.expect("No Metadata").len());
-    } else {
-        println!("{file_inside}");
-    }
+    println!("{file_inside}");
 }

d5448aab952aa4994f9337d4b3096bbe8b010f1d
Tsukasa Sato <tsukasasatodesu@gmail.com>
2024-02-09 17:29:40 +0200

    added new argument and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# ufr
+
+useable file reader.
+
+A file reader I made in 5 minutes
+
+
+## How to use
+
+`ufr <file path> <arguments>`
+
+current file arguments:
+-r     Checks if the file exists
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = args[1].parse::<String>().unwrap();
+    let arg2 = args[2].parse::<String>().unwrap();
     let file_inside = fs::read_to_string(file_path).expect("No File Detected In Specified Path");
 
-    println!("{file_inside}");
+    if arg2 == "-r" {
+        println!("File Exists")
+    } else {
+        println!("{file_inside}");
+    }
 }
